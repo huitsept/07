49f24aa1efac7d80ae3ed466067616031ac921a6
huitsept <70679674+huitsept@users.noreply.github.com>
2020-10-15 16:36:25 +0900

    td 04

--- main.c
+++ main.c
@@ -2,25 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void sub(void);
-
-int main(int argc, char *argv[]) {
-	
-	int i;
-	for (i=0; i<5; i++) {
-		sub();
+void f(void);
+int i;
+int main(void) {
+	for(i=0; i<5; i++)
+	{
+		f();
 	}
 	return 0;
 }
 
-void sub(void){
-	
-	int auto_count = 0;
-	static int static_count=0;
-	
-	auto_count++;
-	static_count++;
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n", static_count);
-
+void f(void)
+{
+	for (i=0; i<10; i++)
+		printf("#");
 }
